013cb43671dc7a4ef23d6c779aeeea6a4645a3eb
Abdullah AL Otaibi <s202322890@kfupm.edu.sa>
2026-04-15 11:48:10 +0300

    task 1 done

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
-// import dotenv and load environment variables from .env
+import dotenv from "dotenv";
+dotenv.config();
 
-
-import { connectDB } from "./db.js";
 import { Song } from "./models/song.model.js";
 
 const app = express();
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 5174;
 app.use(cors());              
 app.use(express.json());
 
-await connectDB(process.env.MONGO_URL);
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Mongo connected");
+} catch (err) {
+  console.error("Connection error:", err.message);
+}
 
 // api/songs (Read all songs)
 
